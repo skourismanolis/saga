168fbfab842f5acdd9fa8d277932da3f2756a4d9
Thomas Papathimiopoulos <thomaspapathimiopoylos@gmail.com>
2021-05-25 21:08:46 +0300

    set up App.vue for Footer

--- sources/front/src/App.vue
+++ sources/front/src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+   <div id="app">
+        <Footer/>
     </div>
-    <router-view />
-  </div>
 </template>
+
+<script>
+import Footer from './components/Footer';
+
+export default {
+	components: {
+        Footer,
+	},
+};
+</script>
+
 
 <style lang="scss">
 #app {
---
+++ sources/front/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row">
+        <div class="col">
+            <a class="t my-2">Εμπειρίες</a>
+        </div>
+        <div class="col">
+            <a class="t my-2">Εμπειρίες</a>
+        </div>
+        <div class="col">
+            <a class="t my-2">Εμπειρίες</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container{
+    height: 261px;
+    background-color: #31245D;
+
+    color: white;
+}
+</style>
