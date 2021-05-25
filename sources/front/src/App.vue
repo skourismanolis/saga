0244138c253821ca86416bb254ac8d30f53002a6
Thomas Papathimiopoulos <thomaspapathimiopoylos@gmail.com>
2021-05-25 23:10:24 +0300

    footer finished

--- sources/front/src/App.vue
+++ sources/front/src/App.vue
@@ -16,24 +16,12 @@ export default {
 
 
 <style lang="scss">
+// GLOBAL CSS
+
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+	font-family: 'Roboto', sans-serif;
+    background-color: #31245D;
 }
 </style>
