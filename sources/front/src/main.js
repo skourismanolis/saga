2eecccd5ccb95ca55a3ffeffe59cdc68b1f6e67d
Thomas Papathimiopoulos <thomaspapathimiopoylos@gmail.com>
2021-05-26 19:59:32 +0300

    downgraded bootstrap to work with custom scss

--- sources/front/src/main.js
+++ sources/front/src/main.js
@@ -3,13 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './scss/custom.scss'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-
 
 new Vue({
   router,
