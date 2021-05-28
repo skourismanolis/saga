c76886e0522d7353000506f4a57e42f3e4bdf646
evita <ev.sinod@gmail.com>
2021-05-28 12:02:57 +0300

    navbar logged in u/c

---
+++ sources/front/src/components/navbar_off.vue
@@ -0,0 +1,47 @@
+<template>
+	<div>
+		<b-navbar>
+			<img src="@/assets/logo.png" id="sagalogo" alt="" />
+			<div class="rcorner_top">
+				<div>
+					<b-button href="#">Σύνδεση / Εγγραφή</b-button>
+				</div>
+			</div>
+		</b-navbar>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'NavBar',
+};
+</script>
+
+<style scoped>
+b-navbar {
+	justify-content: none;
+}
+
+.rcorner_top {
+	/*display: inline;
+	text-align: center;*/
+	display: flex;
+	align-items: center;
+	padding: 10px;
+	font-size: 18px;
+	color: #e5e5e5;
+}
+a {
+	color: #e5e5e5;
+}
+
+#sagalogo {
+	bottom: 0;
+}
+
+.navbar-expand {
+	justify-content: space-between;
+	margin: 0;
+	padding-bottom: 0;
+}
+</style>
