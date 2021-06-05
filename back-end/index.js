5d8f7b9554128d65b16206a6c4ea20ac1b724517
KSiotis <siotiskostas@gmail.com>
2021-06-05 03:23:08 +0300

    fixed dependency, reorganized for testing, updated "yarn dev"

--- back-end/index.js
+++ back-end/index.js
@@ -1,15 +1,11 @@
 require('dotenv').config();
 
-const port = 8080;
 const express = require('express');
 const app = express();
 app.use(express.json());
 const cors = require('cors');
 app.use(cors());
 const jwt = require('jsonwebtoken');
-
-// const { connect, db } = require('./db');
-const connect = require('./db').connect;
 
 app.use('/', (req, res, next) => {
 	const header = req.get('authorization');
@@ -31,23 +27,10 @@ app.use('/', (req, res, next) => {
 });
 
 app.get('/', async (req, res) => {
-	// const [result] = await db.pool.query('SELECT * FROM user');
 	res.status(200).send('Welcome to Saga');
 });
 
 app.use('/users', require('./routes/Users'));
 app.use('/token', require('./routes/Token'));
 
-async function main() {
-	try {
-		await connect();
-	} catch (error) {
-		console.error(error);
-		return;
-	}
-	app.listen(port, () =>
-		console.log(`App listening at http://localhost:${port}`)
-	);
-}
-
-main();
+module.exports = app;
--- back-end/tests/Unit/token.spec.js
+++ back-end/tests/Unit/token.spec.js
@@ -1,15 +1,13 @@
 const app = require('../../index');
 
-describe('Filter function', () => {
-	test('it should filter by a search term (link)', () => {
-		const input = [
-			{ id: 1, url: 'https://www.url1.dev' },
-			{ id: 2, url: 'https://www.url2.dev' },
-			{ id: 3, url: 'https://www.link3.dev' },
-		];
+const supertest = require('supertest');
+const request = supertest(app);
 
-		const output = [{ id: 3, url: 'https://www.link3.dev' }];
+describe('Token handling testing functions', () => {
+	test('GET /', async () => {
+		const response = await request.get('/');
 
-		expect(input).toEqual(output);
+		expect(response.status).toBe(200);
+		expect(response.text).toBe('Welcome to Saga');
 	});
 });
