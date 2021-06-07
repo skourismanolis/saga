e6ea37af07787ec07c969f80c7df00dcf0d3306b
evita <ev.sinod@gmail.com>
2021-06-07 18:19:08 +0300

    horizontal list

---
+++ front-end/src/components/MemberList.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<ul id="team">
+			<li v-for="item in printArray" :key="item">
+				{{ item }}
+			</li>
+			<li v-if="printArray.length > 4">more</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		members: Array,
+	},
+	computed: {
+		printArray() {
+			return this.members.slice(0, 4);
+		},
+	},
+};
+</script>
+
+<style>
+ul {
+	list-style: none;
+	list-style-type: none;
+}
+
+li {
+	display: inline;
+	float: left;
+}
+
+#team {
+	display: table;
+	margin: 0 auto;
+}
+</style>
