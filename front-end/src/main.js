532245780766179407f777c8794e26185ef93ef1
Thomas Papathimiopoulos <thomaspapathimiopoylos@gmail.com>
2021-06-20 13:45:32 +0300

    imported api-client to front-end

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -4,12 +4,16 @@ import router from './router';
 import store from './store';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import './scss/custom.scss';
+import ApiClient from '@dira/api-client';
+
 // import 'bootstrap/dist/css/bootstrap.css'
 // import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+
+Vue.prototype.$saga_client = new ApiClient({ url: 'http://localhost:3000' });
 
 new Vue({
 	router,
