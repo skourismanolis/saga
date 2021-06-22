ef5de899bf7f229953f6bceac49687fdce3b5fd1
Thomas Papathimiopoulos <thomaspapathimiopoylos@gmail.com>
2021-06-22 13:52:57 +0300

    added staying active events to project-navbar

--- front-end/src/components/ProjectNavbar.vue
+++ front-end/src/components/ProjectNavbar.vue
@@ -1,19 +1,42 @@
 <template>
 	<div class="d-flex justify-content-around">
-		<button type="button" class="btn btn-light button-container">
+		<button
+			type="button"
+			class="btn btn-light button-container"
+			@click="$emit('tab-change', 'backlog')"
+		>
 			Backlog
-			<div class="line align-self-end"></div>
+			<div class="line" v-bind:class="{ active: tab == 'backlog' }"></div>
 		</button>
-		<button type="button" class="btn btn-light button-container">
+		<button
+			type="button"
+			class="btn btn-light button-container"
+			@click="$emit('tab-change', 'board')"
+		>
 			Board
-			<div class="line"></div>
+			<div class="line" v-bind:class="{ active: tab == 'board' }"></div>
 		</button>
-		<button type="button" class="btn btn-light button-container">
+		<button
+			type="button"
+			class="btn btn-light button-container"
+			@click="$emit('tab-change', 'settings')"
+		>
 			Project Settings
-			<div class="line"></div>
+			<div
+				class="line"
+				v-bind:class="{ active: tab == 'settings' }"
+			></div>
 		</button>
 	</div>
 </template>
+
+<script>
+export default {
+	props: {
+		tab: String,
+	},
+};
+</script>
 
 <style scoped>
 .button-container {
@@ -35,6 +58,10 @@
 }
 
 .button-container:hover .line {
+	background-color: #6654a1;
+}
+
+.active {
 	background-color: #564787;
 }
 </style>
