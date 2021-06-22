afba200f2183bd5aee69c62e9d4ce56efec9dfb8
evita <ev.sinod@gmail.com>
2021-06-22 20:34:53 +0300

    memberlist before backend req

--- front-end/src/components/MemberList.vue
+++ front-end/src/components/MemberList.vue
@@ -4,8 +4,11 @@
 			<li v-for="item in printArray" :key="item">
 				{{ item }}
 			</li>
-			<li v-if="printArray.length > 4">more</li>
+			<li v-if="members.length > 4">
+				<i class="bi bi-three-dots"></i>
+			</li>
 		</ul>
+		<img src="https://pfpmaker.com/_nuxt/img/profile-3-1.3e702c5.png" />
 	</div>
 </template>
 
@@ -22,19 +25,32 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 ul {
 	list-style: none;
 	list-style-type: none;
+	overflow: hidden;
 }
 
 li {
 	display: inline;
 	float: left;
+	border-radius: 15px;
 }
 
 #team {
 	display: table;
 	margin: 0 auto;
+}
+
+img {
+	vertical-align: middle;
+	border-style: none;
+	width: 26px;
+	height: auto;
+}
+
+#img2 {
+	margin-left: -6px;
 }
 </style>
