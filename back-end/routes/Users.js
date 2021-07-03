b0aad13d3668a5680da04a209e84982010bc1721
Manolis Skouris <44413349+skourismanolis@users.noreply.github.com>
2021-07-03 21:21:07 +0300

    User register (#77)
    * use SMTP and gmx email
    
    * change rateplans name
    
    * make register functional

--- back-end/routes/Users.js
+++ back-end/routes/Users.js
@@ -147,10 +147,11 @@ app.post('/', async (req, res) => {
 				process.env.EMAIL_SECRET
 			);
 
-			const url = `http://localhost:8080/token/${emailToken}`;
+			const url = `http://localhost:3000/token/${emailToken}`;
 
 			var transporter = nodemailer.createTransport({
-				service: 'gmail', // hostname
+				host: 'mail.gmx.com',
+				port: 465,
 				auth: {
 					user: process.env.GMAIL_EMAIL,
 					pass: process.env.GMAIL_PASS,
@@ -158,6 +159,7 @@ app.post('/', async (req, res) => {
 			});
 
 			await transporter.sendMail({
+				from: process.env.GMAIL_EMAIL,
 				to: result[0].email,
 				subject: 'Confirm Email for Saga Account',
 				html: `Please click this link to confirm your email: <a href="${url}">${url}</a>`,
