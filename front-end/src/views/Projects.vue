4d2fdef7b6c279d76a2c1914e69e4b892c1119dd
Manolis Skouris <skourismanolis@gmail.com>
2021-07-03 22:30:48 +0300

    add pagination to project list

--- front-end/src/views/Projects.vue
+++ front-end/src/views/Projects.vue
@@ -5,81 +5,99 @@
 			<span class="purple-text">project</span>
 			<span class="black-text">μου</span>
 		</div>
-		<span v-if="projects.length == 0" class="empty-projects-message"
-			>Δεν έχεις ξεκινήσει κανένα project ακόμα, φτιάξε το πρώτο σου
-			project τώρα!
-		</span>
-		<div class="d-flex flex-row buttons-search-container mb36">
-			<button
-				type="button"
-				class="btn btn-primary d-flex align-items-center create-button"
-				v-b-modal.create-project-modal
-			>
-				Δημιουργία
-				<i class="bi bi-plus align-self-center"></i>
-			</button>
+		<div v-if="projects == null"><b-spinner /></div>
+		<div v-else>
+			<span v-if="projects.total == 0" class="empty-projects-message"
+				>Δεν έχεις ξεκινήσει κανένα project ακόμα, φτιάξε το πρώτο σου
+				project τώρα!
+			</span>
+			<div class="d-flex flex-row buttons-search-container mb36">
+				<button
+					type="button"
+					class="
+						btn btn-primary
+						d-flex
+						align-items-center
+						create-button
+					"
+					v-b-modal.create-project-modal
+				>
+					Δημιουργία
+					<i class="bi bi-plus align-self-center"></i>
+				</button>
 
-			<!-- modal -->
-			<b-modal
-				id="create-project-modal"
-				cancel-title="Ακύρωση"
-				cancel-variant="danger"
-				ok-title="Αποθήκευση"
-				@ok="createProject"
-			>
-				<!-- header -->
-				<template #modal-header="{}">
-					<div>
-						<span class="black-text-modal">Δημιουργία</span>
-						<span class="purple-text-modal">project</span>
+				<!-- modal -->
+				<b-modal
+					id="create-project-modal"
+					cancel-title="Ακύρωση"
+					cancel-variant="danger"
+					ok-title="Αποθήκευση"
+					@ok="createProject"
+				>
+					<!-- header -->
+					<template #modal-header="{}">
+						<div>
+							<span class="black-text-modal">Δημιουργία</span>
+							<span class="purple-text-modal">project</span>
+						</div>
+					</template>
+
+					<!-- content -->
+					<label for="exampleInputEmail1">Τίτλος Project</label>
+					<b-input
+						type="text"
+						class="mb12"
+						v-model="new_project.title"
+						placeholder="Εισάγετε τίτλο..."
+					/>
+					<div class="d-flex justify-content-center">
+						<img src="../assets/plant.png" />
 					</div>
-				</template>
+				</b-modal>
 
-				<!-- content -->
-				<label for="exampleInputEmail1">Τίτλος Project</label>
-				<b-input
-					type="text"
-					class="mb12"
-					v-model="new_project.title"
-					placeholder="Εισάγετε τίτλο..."
-				/>
-				<div class="d-flex justify-content-center">
-					<img src="../assets/plant.png" />
-				</div>
-			</b-modal>
-
-			<div class="input-group search-field" v-if="projects.length > 0">
-				<input
-					type="text"
-					class="form-control"
-					placeholder="Αναζήτηση..."
-				/>
-				<div class="input-group-append">
-					<button class="btn btn-primary" type="button">
-						<i class="bi bi-search"></i>
-					</button>
+				<div
+					class="input-group search-field"
+					v-if="projects.length > 0"
+				>
+					<input
+						type="text"
+						class="form-control"
+						placeholder="Αναζήτηση..."
+					/>
+					<div class="input-group-append">
+						<button class="btn btn-primary" type="button">
+							<i class="bi bi-search"></i>
+						</button>
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<img
-			v-if="projects.length == 0"
-			class="align-self-center"
-			src="../assets/empty-projects-art.png"
-		/>
-		<div v-else class="list-container">
-			<div class="mb12">
-				<span class="title-label">Τίτλος</span>
-				<span class="admins-label">Διαχειριστές</span>
-				<span class="members-label">Μέλη</span>
-			</div>
-			<ProjectRow
-				class="project-row"
-				v-bind:class="{ oddrow: index % 2 != 0 }"
-				v-for="(project, index) in projects"
-				:key="index"
-				:project="project"
+			<img
+				v-if="projects.total == 0"
+				class="align-self-center"
+				src="../assets/empty-projects-art.png"
 			/>
+			<div v-else class="list-container">
+				<div class="mb12">
+					<span class="title-label">Τίτλος</span>
+					<span class="admins-label">Διαχειριστές</span>
+					<span class="members-label">Μέλη</span>
+				</div>
+				<ProjectRow
+					class="project-row"
+					:class="{ oddrow: index % 2 != 0 }"
+					v-for="(project, index) in projects.content"
+					:key="index"
+					:project="project"
+				/>
+				<div class="d-flex justify-content-center mt-4">
+					<b-pagination
+						v-model="currentPage"
+						:total-rows="projects.total"
+						:per-page="projects.perPage"
+					/>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -95,75 +113,21 @@ export default {
 			new_project: {
 				title: '',
 			},
-
-			projects: [
-				{
-					pic: require('../assets/profile pics/default-profile-pic.png'),
-					name: 'Example Project',
-					admins: [
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-					],
-					members: [
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-					],
-				},
-				{
-					pic: require('../assets/profile pics/default-profile-pic.png'),
-					name: 'Example Project',
-					admins: [
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-					],
-					members: [
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-					],
-				},
-				{
-					pic: require('../assets/profile pics/default-profile-pic.png'),
-					name: 'Example Project',
-					admins: [
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-					],
-					members: [
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-						require('../assets/profile pics/default-profile-pic.png'),
-					],
-				},
-			],
+			projects: null,
 		};
 	},
-	computed: {},
+	computed: {
+		currentPage: {
+			get: function () {
+				if (this.projects != null) return this.projects.currentPage + 1;
+				else return 1;
+			},
+			set: async function (page) {
+				if (this.projects != null)
+					await this.projects.setPage(page - 1);
+			},
+		},
+	},
 	methods: {
 		async createProject() {
 			let project = await this.$client.createProject({
@@ -171,6 +135,9 @@ export default {
 			});
 			this.$router.push(`/projects/${project.id}/settings`);
 		},
+	},
+	async created() {
+		this.projects = await this.$client.getProjects();
 	},
 };
 </script>
