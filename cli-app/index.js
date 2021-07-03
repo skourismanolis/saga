dfa5dea86656f223df19e9e22bf29d4d31c021ec
KSiotis <siotiskostas@gmail.com>
2021-07-03 14:26:55 +0300

    added login, register, logout

--- cli-app/index.js
+++ cli-app/index.js
@@ -2,20 +2,32 @@
 
 const { Command } = require('commander');
 const program = new Command();
+const keytar = require('keytar');
+
+const domain = 'http://127.0.0.1';
+const port = 8080;
+const SagaClient = require('@dira/api-client');
+let client = new SagaClient({ url: domain+':'+port });
+
+module.exports = {
+	client,
+};
 
 const User = require('./src/User');
 
 program.version('1.0.0');
-
 const timeLabel = 'command duration';
 program
 	.option('-p, --profile', 'show how long command takes')
-	.hook('preAction', (thisCommand) => {
+	.hook('preAction', async (thisCommand) => {
 		if (thisCommand.opts().profile) {
 			console.time(timeLabel);
 		}
+		await keytar.getPassword('Saga', 'Saga').then((result) => {
+			if (result) client.setToken(result);
+		});
 	})
-	.hook('postAction', (thisCommand) => {
+	.hook('postAction', async (thisCommand) => {
 		if (thisCommand.opts().profile) {
 			console.timeEnd(timeLabel);
 		}
@@ -23,6 +35,7 @@ program
 
 //user
 program.addCommand(User.login);
+program.addCommand(User.logout);
 program.addCommand(User.register);
 
-program.parse(process.argv);
+program.parseAsync(process.argv);
--- cli-app/src/User.js
+++ cli-app/src/User.js
@@ -1,23 +1,56 @@
 const { Command } = require('commander');
 const program = new Command();
+const keytar = require('keytar');
+
+const { client } = require('../index');
 
 const login = program
 	.command('login <email> <password>')
 	.description('login to your account')
 	.action(async (email, password) => {
-		console.log(email);
-		console.log(password);
+		try {
+			await client.login({ email, password });
+			await keytar.setPassword('Saga', 'Saga', client._token);
+		} catch (error) {
+			console.error(error.response.data);
+		}
+	});
+
+const logout = program
+	.command('logout')
+	.description('logout of your account')
+	.action(async () => {
+		try {
+			await keytar.deletePassword('Saga', 'Saga');
+		} catch (error) {
+			console.error(error.response.data);
+		}
 	});
 
 const register = program
-	.command('register <name> <email> <password>')
+	.command('register <email> <password> <name> <surname> <plan> [username]')
 	.description('register a new account')
-	.action(async (email, password) => {
-		console.log(email);
-		console.log(password);
+	.action(async (email, password, name, surname, plan, username) => {
+		username = username || null;
+		try {
+			await client.register({
+				email,
+				password,
+				name,
+				surname,
+				plan,
+				username,
+			});
+			console.log(
+				'Registration Complete! We have sent you a verification email'
+			);
+		} catch (error) {
+			console.error(error.response.data);
+		}
 	});
 
 module.exports = {
 	login,
+	logout,
 	register,
 };
