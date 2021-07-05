e12aff9c49caf7a9be89db9c89c7d8aad4cf8185
KSiotis <siotiskostas@gmail.com>
2021-07-05 04:36:36 +0300

    updated setup.js, tweaked runQuery

--- back-end/tests/setup.js
+++ back-end/tests/setup.js
@@ -7,27 +7,61 @@ const request = supertest(app);
 module.exports = async function setup() {
 	await connect();
 	await runQuery(db.pool, [
-		'DELETE FROM member WHERE idProject = 2',
+		//remove all members
+		'DELETE FROM member',
+		//remove all sprints
 		'UPDATE project SET activeSprint = NULL WHERE idProject > 0',
-		'DELETE FROM sprint WHERE idSprint > 0',
-		'ALTER TABLE sprint AUTO_INCREMENT = 1',
-		'DELETE FROM `column` WHERE idColumn > 0',
-		'ALTER TABLE `column` AUTO_INCREMENT = 1',
-		'DELETE FROM epic WHERE idEpic > 0',
-		'ALTER TABLE epic AUTO_INCREMENT = 1',
-		'DELETE FROM project WHERE idProject > 0',
-		'ALTER TABLE project AUTO_INCREMENT = 1',
-		'DELETE FROM user WHERE idUser > 0',
-		'ALTER TABLE user AUTO_INCREMENT = 1',
+		'UPDATE issue SET idSprint = NULL',
+		'DELETE FROM sprint',
+		//remove all extra columns
+		'UPDATE issue SET idColumn = NULL',
+		'DELETE FROM `column` WHERE `order` > 2',
+		//remove all epics
+		'UPDATE issue SET idEpic = NULL',
+		'DELETE FROM epic',
+		//remove all labels
+		'UPDATE issue SET idLabel = NULL',
 		'DELETE FROM label WHERE idLabel > 0',
+		//remove assignees
+		'DELETE FROM assignee',
+		//remove comments
+		'DELETE FROM comment',
+		//remove issues
+		'DELETE FROM issue',
+		//remove columns
+		'DELETE FROM `column`',
+		//remove projects
+		'DELETE FROM project',
+		//remove payments
+		'DELETE FROM payment',
+		//remove users
+		'DELETE FROM user WHERE idUser > 0',
+		//resetting auto increments
+		'ALTER TABLE assignee AUTO_INCREMENT = 1',
+		'ALTER TABLE `column` AUTO_INCREMENT = 1',
+		'ALTER TABLE comment AUTO_INCREMENT = 1',
+		'ALTER TABLE epic AUTO_INCREMENT = 1',
+		'ALTER TABLE issue AUTO_INCREMENT = 1',
 		'ALTER TABLE label AUTO_INCREMENT = 1',
+		'ALTER TABLE member AUTO_INCREMENT = 1',
+		'ALTER TABLE payment AUTO_INCREMENT = 1',
+		'ALTER TABLE project AUTO_INCREMENT = 1',
+		'ALTER TABLE sprint AUTO_INCREMENT = 1',
+		'ALTER TABLE user AUTO_INCREMENT = 1',
+
+		//starting values, order matters
 		'INSERT INTO project (idProject, title, picture, activeSprint) VALUES (2,"asdasd", null, null)',
 		'INSERT INTO label (idLabel,idProject,name,color) VALUES (1,2,"Frontend","#123456")',
+		'INSERT INTO label (idLabel,idProject,name,color) VALUES (2,2,"CLI","#000000")',
+		'INSERT INTO label (idLabel,idProject,name,color) VALUES (3,2,"Backend","#FF00FF")',
 		'INSERT INTO sprint (idSprint, idProject, title, deadline) VALUES (1,2,"sabsab","2023-07-07")',
+		'INSERT INTO sprint (idSprint, idProject, title, deadline) VALUES (2,2,"sprint_2",null)',
 		'INSERT INTO `column`  (idProject, name, `order`) VALUES (2, "TO-DO", 1)',
 		'INSERT INTO `column` (idProject, name, `order`) VALUES (2, "IN PROGRESS", 2)',
 		'INSERT INTO epic (idEpic, idProject, title, `start`, deadline, description) VALUES (1, 2, "test_epic", null,"2023-07-07", "test_description")',
-		'INSERT INTO issue (code, idProject, title, category, points, priority, deadline, description, idLabel, idSprint) VALUES ("ab",2,"test_title","Story","2","Low","2022-01-01","test",null,1)',
+		'INSERT INTO epic (idEpic, idProject, title, `start`, deadline, description) VALUES (2, 2, "test_epic_2", null,"2022-07-07", "test_description_2")',
+		'INSERT INTO issue (code, idProject, title, category, points, priority, deadline, description, idLabel, idSprint, idColumn) VALUES ("ab",2,"test_title","Story","2","Low","2022-01-01","test",null,1,1)',
+		'INSERT INTO issue (code, idProject, title, category, points, priority, deadline, description, idLabel, idSprint, idColumn) VALUES ("2F3D",2,"lorem","Task",12,"Neutral",null,"lorem ipsum dolor sit amet",1,2,2)',
 	]);
 	await request.post('/users/').send({
 		username: 'admin',
@@ -47,11 +81,25 @@ module.exports = async function setup() {
 		plan: 'Free',
 	});
 
+	await request.post('/users/').send({
+		username: 'test_member_2',
+		email: 'random_user@test.com',
+		password: 'test_member',
+		name: 'test_member_2',
+		surname: 'test_member_2',
+		plan: 'Free',
+	});
+
 	await runQuery(db.pool, [
 		'UPDATE user SET verified=1 WHERE email="admin@admin.com"',
 		'UPDATE user SET verified=1 WHERE email="test_member@test.com"',
+		'UPDATE user SET verified=1 WHERE email="random_user@test.com"',
 		'INSERT INTO member (idUser,idProject,role) VALUES (1,2,"Admin")',
 		'INSERT INTO member (idUser,idProject,role) VALUES (2,2,"Member")',
+		'INSERT INTO member (idUser,idProject,role) VALUES (3,2,"Member")',
+		'INSERT INTO assignee (code,idUser) VALUES ("2F3D",1)',
+		'INSERT INTO assignee (code,idUser) VALUES ("2F3D",2)',
+		'INSERT INTO assignee (code,idUser) VALUES ("2F3D",3)',
 	]);
 	await db.pool.end();
 };
